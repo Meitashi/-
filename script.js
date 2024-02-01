acc226e80592d46068b2f8265ad0515b3da2a266
Meitashi <138116123+Meitashi@users.noreply.github.com>
2024-02-01 15:01:08 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const result = document.querySelector(".result");
+const coin = document.querySelector(".coin");
+const button = document.querySelector("button");
+let animation = false;
+
+button.addEventListener("click", toss);
+
+function toss(){
+    let random = Math.floor(Math.random() * 2);
+    result.innerHTML = "";  
+    coin.style.backgroundImage =`url(/coin-flip-30.gif)`
+    setTimeout(() => {
+        coin.style.backgroundImage =`url(/coin-flip-30.png)`
+        if(random == 0){
+            result.innerHTML = "Решка";
+        }else{
+            result.innerHTML = "Орёл";
+        }
+    }, 1000);
+}
